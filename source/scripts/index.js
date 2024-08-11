76cb4b419309d737eb366a0b8fd55f390d304ef4
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2024-08-11 19:17:04 +0700

    Сетки header, hero. добалвено событие принажатии кнопки

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
 /* в этот файл добавляет скрипты*/
+const navMobile = document.querySelector('.navigation');
+const buttonToggle = document.querySelector('.page-header__nav-toggle');
+
+buttonToggle.onclick = function() {
+  navMobile.classList.toggle('close-button');
+};
