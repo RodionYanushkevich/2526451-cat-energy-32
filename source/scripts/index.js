6677850d5184ce99b017349220cca6998a3e176f
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2024-08-11 22:07:50 +0700

    рефакторинг

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,7 +1,12 @@
-/* в этот файл добавляет скрипты*/
 const navMobile = document.querySelector('.navigation');
 const buttonToggle = document.querySelector('.page-header__nav-toggle');
 
 buttonToggle.onclick = function() {
   navMobile.classList.toggle('close-button');
+  if (buttonToggle.classList.contains('cross')) {
+    buttonToggle.classList.remove('cross');
+    buttonToggle.classList.add('burger');
+  }else{
+    buttonToggle.classList.add('cross');
+  }
 };
