3450cce8b01442cd0a7306e54deb1f0fca679e1c
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2024-08-18 20:36:49 +0700

    рефакторинг

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,6 @@
 const navMobile = document.querySelector('.page-header_nav');
 const buttonToggle = document.querySelector('.page-header__nav-toggle');
-
+// Поставить окно окрытым если не работает ява
 buttonToggle.onclick = function() {
   navMobile.classList.toggle('page-header_nav--state-open');
   if (buttonToggle.classList.contains('page-header__nav-toggle--cross')) {
