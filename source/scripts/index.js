af94b63936c0bb9f9f7c6cc69c11a9bdcd69f6a4
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2024-08-19 15:48:05 +0700

    Карта

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,11 @@
 const navMobile = document.querySelector('.page-header_nav');
 const buttonToggle = document.querySelector('.page-header__nav-toggle');
-// Поставить окно окрытым если не работает ява
+
+navMobile.classList.remove('page-header_nav--state-open');
+buttonToggle.classList.remove('page-header__nav-toggle--cross');
+buttonToggle.classList.add('page-header__nav-toggle--burger');
+
+
 buttonToggle.onclick = function() {
   navMobile.classList.toggle('page-header_nav--state-open');
   if (buttonToggle.classList.contains('page-header__nav-toggle--cross')) {
